df9bc2e161c4e1959f193625f238aeba7b4ff415
David Olejniczak <david.olejniczak@icloud.com>
2026-01-17 20:44:43 -0500

    Initial Commit

---
+++ SR25Controls/SR25ControlsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SR25ControlsApp.swift
+//  SR25Controls
+//
+//  Created by David Olejniczak on 2026-01-17.
+//
+
+import SwiftUI
+
+@main
+struct SR25ControlsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SR25ControlsTests/SR25ControlsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SR25ControlsTests.swift
+//  SR25ControlsTests
+//
+//  Created by David Olejniczak on 2026-01-17.
+//
+
+import Testing
+@testable import SR25Controls
+
+struct SR25ControlsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
